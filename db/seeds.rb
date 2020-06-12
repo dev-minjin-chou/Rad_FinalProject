7203fe457a6e92fc52e04e1b818e200a0af61c29
Minjin chou <49186771+kuntakinte777@users.noreply.github.com>
2020-06-13 06:29:30 +1000

    final projects

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,37 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Clock.create!(value: "AUS Eastern standard Time", abbr: "AEST",
+    offset: 10,
+    isdst: "false",
+    text: "(UTC+10:00) Canberra, Sydney, Melbourne",
+    utc: "Australia/Melbourne",
+    city: "Melbourne")
+
+Clock.create!(value: "Paraguay Standard Time", abbr: "PYT",
+    offset: -4,
+    isdst: "false",
+    text: "(UTC-04:00) Asuncion",
+    utc: "Asuncion",
+    city: "America/Asuncion")
+
+Clock.create!(value: "Kamchatka Standard Time", abbr: "KDT",
+    offset: 13,
+    isdst: "true",
+    text: "(UTC+12:00) Petropavlovsk-Kamchatsky - Old",
+    utc: "Asia/Kamchatka",
+    city: "Kamchatka")
+
+Clock.create!(value: "Hawaiian Standard Time", abbr: "HST",
+    offset: -10,
+    isdst: "false",
+    text: "(UTC-10:00) Hawaii",
+    utc: "Pacific/Honolulu",
+    city: "Honolulu")
+
+Clock.create!(value: "Pacific Daylight Time", abbr: "PDT",
+    offset: -7,
+    isdst: "true",
+    text: "(UTC-07:00) Pacific Time (US & Canada)",
+    utc: "America/Los_Angeles",
+    city: "Los Angeles")
